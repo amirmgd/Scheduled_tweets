652b615b800341b8b29bfb315cc82638729926fe
amirmgd <amirhosseinmajidi7@gmail.com>
2022-03-06 11:58:40 +0330

    added current model and modified navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-    add_flash_types :danger, :info, :warning, :success, :messages
+   before_action :set_current_user
+   
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find(session[:user_id])
+        end
+    end
 end
