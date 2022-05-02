b6bf611d7594bda85882f4f6915372ca8609b57d
amirmgd <amirhosseinmajidi7@gmail.com>
2022-05-02 20:34:22 +0430

    new changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :default_twitter, Rails.application.credentials.dig(:default_twitter,:twitter_api_key), Rails.application.credentials.dig(:default_twitter,:twitter_api_secret)
+    provider :twitter, Rails.application.credentials.dig(:default_twitter,:twitter_api_key), Rails.application.credentials.dig(:default_twitter,:twitter_api_secret)
 end
