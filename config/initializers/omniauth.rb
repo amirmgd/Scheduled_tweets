b1600b5f63d9121c6fe212c0050c8f93b3ce8299
amirmgd <amirhosseinmajidi7@gmail.com>
2022-05-05 16:41:01 +0430

    new changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, 'consumer_key', 'consumer_secret'
+    provider :twitter, ENV['consumer_key'], ENV['consumer_secret']
 end
